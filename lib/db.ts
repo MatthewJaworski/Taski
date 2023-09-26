03d67baa362b1a0416c3ea8dc25a5a001ffa4b91
MatthewJaworski <mat.jaw918@onet.pl>
2023-09-27 00:25:22 +0200

    fix: disabled one rule in one line in eslint

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 declare global {
-  var cachedPrisma: PrismaClient;
+  var cachedPrisma: PrismaClient; // eslint-disable-line no-var
 }
 
 let prisma: PrismaClient;
